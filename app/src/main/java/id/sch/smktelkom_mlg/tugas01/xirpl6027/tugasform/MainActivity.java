9785abd47e9ee0367b6bb99cb53afce695714ed2
Naufal Farras <farrasgopal13@gmail.com>
2016-09-11 10:24:38 +0700

    first commit

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl6027/tugasform/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl6027/tugasform/MainActivity.java
@@ -2,12 +2,31 @@ package id.sch.smktelkom_mlg.tugas01.xirpl6027.tugasform;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    EditText etNis;
+    Button bKirim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextnama);
+        etNis = (EditText) findViewById(R.id.editTextNis);
+        bKirim = (Button) findViewById(R.id.buttonKirim);
+
+        bKirim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
